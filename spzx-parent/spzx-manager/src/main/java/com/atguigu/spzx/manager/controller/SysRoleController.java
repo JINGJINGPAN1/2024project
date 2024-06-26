82839aa3c24816f6de84b5e2fd4036963ef2e9ce
Jingjing Pan <pan_jingjing@foxmail.com>
2024-06-26 21:58:41 +0800

    pageInfo

--- spzx-parent/spzx-manager/src/main/java/com/atguigu/spzx/manager/controller/SysRoleController.java
+++ spzx-parent/spzx-manager/src/main/java/com/atguigu/spzx/manager/controller/SysRoleController.java
@@ -1,11 +1,32 @@
 package com.atguigu.spzx.manager.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.atguigu.spzx.manager.service.SysRoleService;
+import com.atguigu.spzx.model.dto.system.SysRoleDto;
+import com.atguigu.spzx.model.entity.system.SysRole;
+import com.atguigu.spzx.model.vo.common.Result;
+import com.atguigu.spzx.model.vo.common.ResultCodeEnum;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/admin/system/sysRole")
 public class SysRoleController {
+    @Autowired
+    private SysRoleService sysRoleService;
+
+    // 角色列表的方法
+    // current : 当前页  limit: 每页显示记录数
+    // sysRoleDto: 条件角色名称对象
+    @PostMapping("/findByPage/{current}/{limit}")
+    public Result findByPage(@PathVariable("current") Integer current,
+                             @PathVariable("limit") Integer limit,
+                             @RequestBody SysRoleDto sysRoleDto){
+        //角色列表的方法
+        PageInfo<SysRole> pageInfo= sysRoleService.findByPage(sysRoleDto, current, limit);
+        return Result.build(pageInfo, ResultCodeEnum.SUCCESS);
+    }
+
 
 }
--- spzx-parent/spzx-manager/src/main/java/com/atguigu/spzx/manager/service/SysRoleService.java
+++ spzx-parent/spzx-manager/src/main/java/com/atguigu/spzx/manager/service/SysRoleService.java
@@ -1,4 +1,11 @@
 package com.atguigu.spzx.manager.service;
 
+import com.atguigu.spzx.model.dto.system.SysRoleDto;
+import com.atguigu.spzx.model.entity.system.SysRole;
+import com.github.pagehelper.PageInfo;
+
 public interface SysRoleService {
+
+    //角色列表的方法
+    PageInfo<SysRole> findByPage(SysRoleDto sysRoleDto, Integer current, Integer limit);
 }
